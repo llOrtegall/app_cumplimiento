bbb89383b06d2cc5db8415d56ad71e72f4571512
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-14 09:32:53 -0500

    Update dependencies and add axios for API requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+import { URL_API } from './utils/contants'
+
 function App() {
+
+  useEffect(() => {
+    axios.get(`${URL_API}/personas`)
+  }, [])
   
   return (
-    <div className='bg-red-200'>
+    <div className='bg-red-400'>
       <h1>Hello World</h1>
     </div>
   )
---
+++ src/utils/contants.ts
@@ -0,0 +1,3 @@
+const URL_API = import.meta.env.VITE_API_URL
+
+export { URL_API }
