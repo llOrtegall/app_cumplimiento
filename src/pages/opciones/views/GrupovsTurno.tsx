14887202b2b2a3c4cdb9c8fe49f67e5b34cb9746
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-16 16:28:33 -0500

    Refactor GrupovsTurno component to add a "Guardar" button for saving changes

--- src/pages/opciones/views/GrupovsTurno.tsx
+++ src/pages/opciones/views/GrupovsTurno.tsx
@@ -66,6 +66,10 @@ export default function GrupovsTurno() {
           ))}
         </fieldset>
 
+        <button type="submit" className="my-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Guardar
+        </button>
+
       </form>
     </section>
   )
--- src/pages/persona/index.tsx
+++ src/pages/persona/index.tsx
@@ -16,53 +16,55 @@ function PersonasView() {
 
   return (
     <section className='flex h-[88vh] overflow-y-auto m-2 rounded-md shadow-md sm:rounded-lg relative'>
-    <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
-      <thead className='text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400 sticky top-0'>
-        <tr>
-          <th className='px-2 py-3 text-center'>
-            Id
-          </th>
-          <th className='px-2 py-3'>
-            N° Identicación
-          </th>
-          <th className='px-2 py-3'>
-            Apellidos
-          </th>
-          <th className='px-2 py-3'>
-            Nombres
-          </th>
-          <th className='px-2 py-3'>
-            Opciones
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          personas.map((p) => (
-            <tr key={p.identificacion} className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
-              <td className='px-1 py-2 text-center'>
-                {p.id}
-              </td>
-              <td className='px-3 py-2 font-medium text-gray-900 dark:text-white'>
-                {p.identificacion}
-              </td>
-              <td className='px-3 py-2'>
-                {p.apellidos}
-              </td>
-              <td className='px-3 py-2'>
-                {p.nombres}
-              </td>
-              <td className='px-3 py-2'>
-                <button className='p-2 bg-blue-500 text-white rounded-md'>
-                  Editar
-                </button>
-              </td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
-  </section>
+      <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400'>
+        <thead className='text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400 sticky top-0'>
+          <tr>
+            <th className='px-2 py-3 text-center'>
+              Id
+            </th>
+            <th className='px-2 py-3'>
+              N° Identicación
+            </th>
+            <th className='px-2 py-3'>
+              Apellidos
+            </th>
+            <th className='px-2 py-3'>
+              Nombres
+            </th>
+            <th className='px-2 py-3'>
+              Opciones
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            personas.map((p) => (
+              <tr key={p.identificacion} className='odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700'>
+                <td className='px-1 py-2 text-center'>
+                  {p.id}
+                </td>
+                <td className='px-3 py-2 font-medium text-gray-900 dark:text-white'>
+                  {p.identificacion}
+                </td>
+                <td className='px-3 py-2'>
+                  {p.apellidos}
+                </td>
+                <td className='px-3 py-2'>
+                  {p.nombres}
+                </td>
+                <td className='px-3 py-2'>
+                  <button className='p-2 bg-blue-500 text-white rounded-md'>
+                    Editar
+                  </button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
+      
+    </section>
   )
 }
 
