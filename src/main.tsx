0f68906458767231b32eb1f37537c6aa8a730a28
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-20 09:32:00 -0500

    Refactor main.tsx to add AuthProvider for authentication

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import { createRoot } from 'react-dom/client'
 import { Router } from './routes'
 import axios from 'axios'
 import './index.css'
+import { AuthProvider } from './auth/AuthContext'
 
 axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')!).render(
-  <ThemeProvider>
-    <RouterProvider router={Router} />
-  </ThemeProvider>
+  <AuthProvider>
+    <ThemeProvider>
+      <RouterProvider router={Router} />
+    </ThemeProvider>
+  </AuthProvider>
 )
