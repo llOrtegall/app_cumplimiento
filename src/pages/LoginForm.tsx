7ef3820651b7e0573f0bdb22b8512923299851e8
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-23 10:19:59 -0500

    Refactor LoginForm component to update layout and styles

--- src/pages/LoginForm.tsx
+++ src/pages/LoginForm.tsx
@@ -35,28 +35,31 @@ function LoginPage() {
   }
 
   return (
-    <section className='bg-gray-50 dark:bg-gray-900'>
+    <section className=''>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
       <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-        <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
-          <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
-            <figure className='flex justify-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'>
+        <div className='w-full bg-transparent/15 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+          <div className='px-14 py-16'>
+            <figure className='flex justify-center mb-6 text-2xl font-semibold text-gray-900 '>
               <img width={180} src='/gane.webp' alt='logo' />
             </figure>
-            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+            <h1 className='text-lg pb-2 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl '>
               Iniciar sesión en tu cuenta
             </h1>
             <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
               <div>
-                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Usuario:</label>
-                <input type='text' className='bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='CP1118******' required value={username} onChange={ev => setUsername(ev.target.value)} />
+                <label className='block mb-2 text-sm font-medium text-gray-900 '>Usuario:</label>
+                <input className='bg-gray-50 rounded-lg  block w-full p-2.5 outline-blue-400'
+                  onChange={ev => setUsername(ev.target.value)} type='text' placeholder='CP1118******' required value={username} />
               </div>
               <div>
-                <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Contraseña</label>
-                <input type='password' placeholder='••••••••' className='bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required value={password} onChange={ev => setPassword(ev.target.value)} />
+                <label className='block mb-2 text-sm font-medium text-gray-900 '>Contraseña</label>
+                <input className='bg-gray-50 rounded-lg  block w-full p-2.5 outline-blue-400' 
+                 onChange={ev => setPassword(ev.target.value)} type='password' placeholder='••••••••' required value={password}   />
               </div>
 
 
-              <button className='w-full px-4 py-2 text-sm font-medium tracking-wider text-white uppercase bg-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 hover:bg-blue-700 active:bg-blue-700'>Iniciar sesión</button>
+              <button className='w-full px-4 py-3 text-sm font-medium tracking-wider text-white uppercase bg-blue-600 rounded-lg    hover:bg-blue-700 active:bg-blue-700'>Iniciar sesión</button>
             </form>
           </div>
         </div>
