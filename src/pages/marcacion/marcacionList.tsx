631d24030e793ae323a7c3dd74350ff91a505ec2
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-24 11:51:08 -0500

    Refactor MarcacionList component to include date range selection and display the number of records

--- src/pages/marcacion/marcacionList.tsx
+++ src/pages/marcacion/marcacionList.tsx
@@ -10,7 +10,7 @@ const MarcacionesList = () => {
 
   useEffect(() => {
     fetchdata();
-  }, [fechaInitial]);
+  }, [fechaInitial, fechaFinal]);
 
   const fetchdata = async () => {
     try {
@@ -37,6 +37,10 @@ const MarcacionesList = () => {
           <label className='w-full'>Fecha Final</label>
           <input type="date" value={fechaFinal} onChange={(e) => setFechaFinal(e.target.value)}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+        </div>
+
+        <div className='flex items-center'>
+          <span className='font-semibold text-xl'>Cantidad Registros: {data?.count}</span>
         </div>
 
         <div className='flex items-center gap-2'>
