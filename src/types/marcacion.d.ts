bb6c9be2ed59174aed0d83e52c35446de833066f
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-09-27 09:28:13 -0500

    chage type for MarcacionSimple

--- src/types/marcacion.d.ts
+++ src/types/marcacion.d.ts
@@ -1,40 +1,41 @@
 export interface Marcacion {
-  id: number;
-  nombres: string;
-  apellidos: string;
-  fecha_marcacion: string;
-  estado_marcacion: string;
-  nombre_dispositivo: string;
-  observacion: string;
-  observacionPersonal: string;
-  id_turno: number;
-  Pais: string;
-  Ciudad: string;
-  Direccion: string;
-  Latitud: string;
-  Longitud: string;
-  id_foto_temota: string;
+  id: number
+  nombres: string
+  apellidos: string
+  fecha_marcacion: string
+  estado_marcacion: string
+  nombre_dispositivo: string
+  observacion: string
+  observacionPersonal: string
+  id_turno: number
+  Pais: string
+  Ciudad: string
+  Direccion: string
+  Latitud: string
+  Longitud: string
+  id_foto_temota: string
 }
 
 interface MarcacionSimple {
-  id: number;
-  nombres: string;
-  apellidos: string;
-  fecha: string;
+  id: number
+  nombres: string
+  apellidos: string
+  fecha: string
   hora: string
-  estado: string;
+  estado: string
 }
 
 export interface MarcacionResponse {
-  marcaciones: MarcacionSimple[];
-  count: number;
+  marcaciones: MarcacionSimple[]
+  count: number
 }
 
 export interface AuditMarcaciones {
-  id: number;
-  nombres: string;
-  apellidos: string;
-  hora: string;
-  estado: string;
-  hora_inicio: string;
+  id: number
+  documento: string
+  nombres: string
+  apellidos: string
+  hora: string
+  estado: string
+  hora_inicio: string
 }
