3af8e10e8fbc3deac5f6ff65311e4729b51f431c
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-10-10 08:32:36 -0500

    agregando componente select y stilos a algunos botones

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,32 @@
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeaderCell, TableRoot, TableRow } from "./components/Table";
-import { Cliente, DataResponse } from "./types/Interfaces";
-import { useEffect, useState } from "react";
+import { Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow } from './components/Table';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/Select';
+import { Cliente, DataResponse } from './types/Interfaces';
+import { RiDatabase2Line } from '@remixicon/react';
+import { useEffect, useState } from 'react';
+
+const data = [
+  {
+    value: '20',
+    label: '20',
+    icon: RiDatabase2Line
+  },
+  {
+    value: '50',
+    label: '50',
+    icon: RiDatabase2Line
+  },
+  {
+    value: '100',
+    label: '100',
+    icon: RiDatabase2Line
+  },
+  {
+    value: '200',
+    label: '200',
+    icon: RiDatabase2Line
+  },
+]
+
 
 function App() {
   const [clients, setClients] = useState<Cliente[]>([]);
@@ -29,52 +55,79 @@ function App() {
 
   return (
     <section className=''>
-      <div className='py-2 px-2 text-end flex justify-around bg-gray-400'>
-        <p>Total Clientes Nuevos: {totalClients}</p>
-        <p>Clientes Visibles: {pageSize}</p>
-      </div>
-      <div className="h-[90vh] overflow-y-auto">
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableHeaderCell>Name</TableHeaderCell>
-                <TableHeaderCell>Documento</TableHeaderCell>
-                <TableHeaderCell>Telefono</TableHeaderCell>
-                <TableHeaderCell>Correo</TableHeaderCell>
-                <TableHeaderCell>Categoría</TableHeaderCell>
-                <TableHeaderCell>Tipo Zona</TableHeaderCell>
-                <TableHeaderCell>Acciones</TableHeaderCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {clients.map((item) => (
-                <TableRow key={item.DOCUMENTO}>
-                  <TableCell>{item.NOMBRES}</TableCell>
-                  <TableCell>{item.DOCUMENTO}</TableCell>
-                  <TableCell>{item.TELEFONO1}</TableCell>
-                  <TableCell>{item.EMAIL}</TableCell>
-                  <TableCell>{item.CATEGORIA}</TableCell>
-                  <TableCell>{item.TIPOZONA}</TableCell>
-                  <TableCell>
-                    <button className="px-2 py-1 text-sm font-medium text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-100 dark:border-gray-800">Editar</button>
-                  </TableCell>
-                </TableRow>
+
+      <section className='flex py-2 justify-around'>
+        <div className='flex items-center gap-2'>
+          <label className='text-sm font-semibold'>Buscar:</label>
+          <input type='text' className='w-[200px] px-1 py-2 text-sm border border-gray-200 rounded-md dark:border-gray-800' />
+          <button className='px-2 py-2 text-sm font-medium text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-100 dark:border-gray-800 hover:bg-green-200 transition-colors'>Buscar</button>
+        </div>
+
+        <div className='flex items-center gap-2'>
+          <label className='text-sm font-semibold'>Mostrar:</label>
+          <Select defaultValue='100'>
+            <SelectTrigger className='mx-auto w-[120px]'>
+              <SelectValue placeholder='Select' />
+            </SelectTrigger>
+            <SelectContent>
+              {data.map((item) => (
+                <SelectItem key={item.value} value={item.value} >
+                  <span className='flex justify-between gap-x-2'>
+                    <item.icon
+                      className='size-4 shrink-0 text-gray-500 dark:text-gray-500'
+                      aria-hidden='true'
+                    />
+                    {item.label}
+                  </span>
+                </SelectItem>
               ))}
-            </TableBody>
-          </Table>
+            </SelectContent>
+          </Select>
+        </div>
+      </section>
+
+      <div className='h-[89vh] overflow-y-auto'>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>Name</TableHeaderCell>
+              <TableHeaderCell>Documento</TableHeaderCell>
+              <TableHeaderCell>Telefono</TableHeaderCell>
+              <TableHeaderCell>Correo</TableHeaderCell>
+              <TableHeaderCell>Categoría</TableHeaderCell>
+              <TableHeaderCell>Tipo Zona</TableHeaderCell>
+              <TableHeaderCell>Acciones</TableHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {clients.map((item) => (
+              <TableRow key={item.DOCUMENTO}>
+                <TableCell>{item.NOMBRES}</TableCell>
+                <TableCell>{item.DOCUMENTO}</TableCell>
+                <TableCell>{item.TELEFONO1}</TableCell>
+                <TableCell>{item.EMAIL}</TableCell>
+                <TableCell>{item.CATEGORIA}</TableCell>
+                <TableCell>{item.TIPOZONA}</TableCell>
+                <TableCell>
+                  <button className='px-2 py-1 text-sm font-medium text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-100 dark:border-gray-800 hover:bg-green-200 transition-colors'>Editar</button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
 
       </div>
 
       <div className='flex items-center justify-center py-2 bg-yellow-50 gap-2'>
         <button disabled={page === 1} onClick={() => setPage((prev) => prev - 1)}
-          className="px-2 py-1 text-sm font-medium text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-100 dark:border-gray-800">
+          className='px-2 py-1 text-sm font-medium text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-100 dark:border-gray-800'>
           Previous
         </button>
 
         <span>{page} de {totalPages}</span>
 
         <button disabled={page === totalPages} onClick={() => setPage((prev) => prev + 1)}
-          className="px-2 py-1 text-sm font-medium text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-100 dark:border-gray-800" >
+          className='px-2 py-1 text-sm font-medium text-gray-800 bg-gray-100 border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-100 dark:border-gray-800' >
           Next
         </button>
 
