1d5c25bfabe8f9be84e7494abd8b8a9c3ffa8824
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-29 15:18:18 -0500

    está implementación funciona sin embargo debemos mejorar el typado y la forma de retornar la data

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,57 @@
+import { PieChart } from '@mui/x-charts/PieChart';
+import { URL_API_DATA } from '../utils/contanst';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface DataCompany {
+  id: number,
+  value: number,
+  label: string
+}
+
+
+interface Data {
+  empresa: string,
+  data: DataCompany[],
+}
+
 export default function Dashboard() {
-  return(
-    <div>
-      <h1>Dashboard</h1>
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    axios.get(`${URL_API_DATA}/getInfo`)
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
+
+  return (
+    <div className='w-full flex'>
+      {
+        data.map((item) => (
+          <div className='flex flex-col justify-center items-center p-2 space-y-2 rounded-md m-2'>
+            <h1 className='text-2xl font-bold'>{item.empresa}</h1>
+            <PieChart
+              series={[{
+                data: item.data,
+                highlightScope: { fade: 'global', highlight: 'item' },
+                faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                innerRadius: 70,
+                outerRadius: 100,
+                cornerRadius: 10,
+              }]}
+              height={200}
+              width={550}
+            />
+          </div>
+
+        ))
+      }
     </div>
   )
 }
--- server/src/controllers/info.controllers.ts
+++ server/src/controllers/info.controllers.ts
@@ -9,27 +9,52 @@ export const getInfo = async (req: Request, res: Response) => {
       when '39627' then 'Multired'
       when '39628' then 'Servired'
       else 'null' end AS EMPRESA, 
-      COUNT(CASE WHEN PREMIO < 15*47065 THEN 1 END) AS menores_15UVT,
-      COUNT(CASE WHEN PREMIO between 15*47065 and 48*47065 THEN 1 END) AS entre_15a48UVT,
-      COUNT(CASE WHEN PREMIO > 48*47065 THEN 1 END) AS Mayores_48UVT
+      COUNT(CASE WHEN PREMIO < 15*47065 THEN 1 END) AS Result1,
+      COUNT(CASE WHEN PREMIO between 15*47065 and 48*47065 THEN 1 END) AS Result2,
+      COUNT(CASE WHEN PREMIO > 48*47065 THEN 1 END) AS Result3
       FROM DETALLEPREMIOSCMP
       WHERE FECHAPAGO=CURDATE()
       GROUP BY FECHAPAGO,ZONA`
     );
 
-    const [result2] = await PowerBi.query(`
-      SELECT FECHAPAGO, case ZONA
-      when '39627' then 'Multired'
-      when '39628' then 'Servired'
-      else 'null' end AS EMPRESA, 
-      TIPOPREMIO, 
-      count(1) CANT 
-      FROM DETALLEPREMIOSCMP
-      WHERE FECHAPAGO=CURDATE()
-      GROUP BY FECHAPAGO,ZONA,TIPOPREMIO;`
-    );
+    const data = [
+      {
+        id: 1, 
+        value: result1[0].Result1,
+        label: ` (${result1[0].Result1}) Menor a 15 UVT`
+      },
+      {
+        id: 2, 
+        value: result1[0].Result2,
+        label: ` (${result1[0].Result2}) Entre 15 y 48 UVT`
+      },
+      {
+        id: 3, 
+        value: result1[0].Result3,
+        label: ` (${result1[0].Result3}) Mayor a 48 UVT`
+      }
+    ]
 
-    res.status(200).json({ result1, result2 });
+    const data2 = [
+      {
+        id: 1, 
+        value: result1[1].Result1,
+        label: ` (${result1[1].Result1}) Menor a 15 UVT`
+      },
+      {
+        id: 2, 
+        value: result1[1].Result2,
+        label: ` (${result1[1].Result2}) Entre 15 y 48 UVT`
+      },
+      {
+        id: 3, 
+        value: result1[1].Result3,
+        label: ` (${result1[1].Result3}) Mayor a 48 UVT`
+      }
+    ]
+
+
+    res.status(200).json([{ empresa: 'Multired', data }, { empresa: 'Servired', data: data2 }]);
   } catch (error) {
     console.log(error);
     res.status(500).json('Internal server error');
