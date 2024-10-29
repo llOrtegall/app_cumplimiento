fbbaeb004c5d0139e8c5a2515be9943fe1085388
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-29 09:27:36 -0500

    agregando en navbar NavLink para ir a ruta dashboard

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -17,17 +17,18 @@ function NavBar() {
   return (
     <nav className='border-r px-2 py-4 flex flex-col justify-between items-center gap-6 h-screen'>
       <div className='flex flex-col items-center gap-2'>
-        <figure className=''>
-          <RiHome2Line size={32} />
-        </figure>
         <ul className=''>
           <li className='flex flex-col gap-2'>
+            <NavLink title='Dashboard' to='dashboard' className={({ isActive }) => isActive ? 'bg-blue-800 text-white rounded-full p-2' : 'bg-gray-200 rounded-full p-2'}>
+              <RiHome2Line size={32} />
+            </NavLink>
             <NavLink title='Todos los clientes' to='/' className={({ isActive }) => isActive ? 'bg-blue-800 text-white rounded-full p-2' : 'bg-gray-200 rounded-full p-2'}>
               <RiGroupLine size={32} />
             </NavLink>
             <NavLink title='Clientes Nuevos' to='clientes-nuevos' className={({ isActive }) => isActive ? 'bg-blue-800 text-white rounded-full p-2' : 'bg-gray-200 rounded-full p-2'}>
               <RiUserAddLine size={32} />
             </NavLink>
+          
           </li>
         </ul>
       </div>
