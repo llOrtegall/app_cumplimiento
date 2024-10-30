850fb6ed11fb07c05a2784b5dbad28f8dbb31fca
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-30 09:58:28 -0500

    Add useOnWindowResize hook and integrate BarChart in Dashboard with new data fetching

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { PieChart } from '@mui/x-charts/PieChart';
 import { URL_API_DATA } from '../utils/contanst';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BarChart } from '../components/BarChart';
 
 interface DataCompany {
   id: number,
@@ -9,20 +10,38 @@ interface DataCompany {
   label: string
 }
 
+interface DataCompany2 {
+  TIPOPREMIO: string
+  CANT: number
+}
 
 interface Data {
   empresa: string,
   data: DataCompany[],
 }
 
+interface Data2 {
+  Multired: DataCompany2[],
+  Servired: DataCompany2[],
+}
+
 export default function Dashboard() {
   const [data, setData] = useState<Data[]>([]);
+  const [data2, setData2] = useState<Data2>();
 
   useEffect(() => {
     axios.get(`${URL_API_DATA}/getInfo`)
       .then(res => {
-        console.log(res.data);
         setData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+    axios.get(`${URL_API_DATA}/getInfo2`)
+      .then(res => {
+        console.log(res.data);
+        setData2(res.data);
       })
       .catch(err => {
         console.log(err);
@@ -31,27 +50,56 @@ export default function Dashboard() {
 
 
   return (
-    <div className='w-full flex'>
-      {
-        data.map((item) => (
-          <div className='flex flex-col justify-center items-center p-2 space-y-2 rounded-md m-2'>
-            <h1 className='text-2xl font-bold'>{item.empresa}</h1>
-            <PieChart
-              series={[{
-                data: item.data,
-                highlightScope: { fade: 'global', highlight: 'item' },
-                faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-                innerRadius: 70,
-                outerRadius: 100,
-                cornerRadius: 10,
-              }]}
-              height={200}
-              width={550}
-            />
-          </div>
+    <div className='w-full flex flex-col'>
+      <section className='flex'>
+        {
+          data.map((item, index) => (
+            <div key={index} className='w-1/2 flex flex-col justify-center items-center p-2 space-y-2 rounded-md m-2'>
+              <h1 className='text-2xl font-bold'>{item.empresa}</h1>
+              <PieChart
+                series={[{
+                  data: item.data,
+                  highlightScope: { fade: 'global', highlight: 'item' },
+                  faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+                  innerRadius: 70,
+                  outerRadius: 100,
+                  cornerRadius: 10,
+                }]}
+                height={200}
+                width={550}
+              />
+            </div>
+          ))
+        }
+      </section>
 
-        ))
+      {
+        data2 && (
+          data2.Multired.length > 0 && (
+            <section className='flex'>
+              <BarChart
+                className='h-80 w-[48%]'
+                data={data2.Multired}
+                index='TIPOPREMIO'
+                categories={['CANT']}
+                yAxisWidth={80}
+                layout='vertical'
+              />
+              <BarChart 
+                className='h-80 w-[48%]'
+                data={data2.Servired}
+                index='TIPOPREMIO'
+                categories={['CANT']}
+                yAxisWidth={80}
+                layout='vertical'
+              />
+            </section>
+
+
+          )
+        )
       }
+
     </div>
   )
 }
