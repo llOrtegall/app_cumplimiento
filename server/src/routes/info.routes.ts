ff0d6ffecb330628f613d8e359ee739086cd2440
llOrtegall <IvanOrtega_97@hotmail.com>
2024-10-30 09:31:03 -0500

    Add getInfo2 route to fetch additional premios data

--- server/src/routes/info.routes.ts
+++ server/src/routes/info.routes.ts
@@ -1,6 +1,8 @@
-import { getInfo } from '../controllers/info.controllers';
+import { getInfo, getInfo2 } from '../controllers/info.controllers';
 import { Router } from "express";
 
 export const infoRouter = Router();
 
 infoRouter.get('/getInfo', getInfo);
+
+infoRouter.get('/getInfo2', getInfo2);
