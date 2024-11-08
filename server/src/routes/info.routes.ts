2c0b03260e6d71ea7685e549c0edba5472fc9fa6
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-08 10:19:15 -0500

    Add route for reportClientesGanadores: integrate client winners report functionality

--- server/src/routes/info.routes.ts
+++ server/src/routes/info.routes.ts
@@ -1,4 +1,4 @@
-import { getInfo, getInfo2, getReportBaloto } from '../controllers/info.controllers';
+import { getInfo, getInfo2, getReportBaloto, getClientesGanadores } from '../controllers/info.controllers';
 import { Router } from "express";
 
 export const infoRouter = Router();
@@ -8,3 +8,5 @@ infoRouter.get('/getInfo', getInfo);
 infoRouter.get('/getInfo2', getInfo2);
 
 infoRouter.post('/reportBaloto', getReportBaloto);
+
+infoRouter.post('/reporClientGanadores', getClientesGanadores);
