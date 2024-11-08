6468002aa2e711f139cf1b711fc980de24eda248
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-08 10:07:43 -0500

    Update SeleccionReportes component: enhance descriptions to include date range selection for reports

--- client/src/pages/SeleccionReportes.tsx
+++ client/src/pages/SeleccionReportes.tsx
@@ -9,8 +9,8 @@ export default function SeleccionReportes() {
 
       <article className='flex flex-col justify-around bg-gradient-to-r from-cyan-900 to-blue-800 p-4 rounded-md shadow-xl space-y-2'>
         <h1 className='font-semibold text-2xl text-white'>Reporte Baloto</h1>
-        <p className='text-white'>
-          Genera la información de los premios pagados con el producto Baloto Miloto y Colorloto.
+        <p className='text-white text-balance'>
+          Genera la información de los premios pagados con el producto Baloto Miloto y Colorloto. Se debe seleccionar rango de fechas para la consulta.
         </p>
         <div className='flex items-center justify-between'>
           <img src="/logoBaloto.webp" alt="logo de baloto" width={120} />
@@ -21,12 +21,12 @@ export default function SeleccionReportes() {
 
       <article className='flex flex-col justify-around bg-gradient-to-r from-green-900 to-green-800 p-4 rounded-md shadow-xl space-y-2'>
         <h1 className='font-semibold text-2xl text-white'>Clientes Más Ganadores</h1>
-        <p className='text-white'>
-          Permite mediante el número de cédula, conocer los clientes que más han ganado en premios.
+        <p className='text-white text-balance'>
+          Permite mediante el número de cédula, conocer los clientes que más han ganado en premios. Se debe seleccionar rango de fechas para la consulta.
         </p>
         <div className='flex items-center justify-between'>
           <RiGroupLine size={48} color='white' />
-          <button 
+          <button onClick={() => navigate('/reportClientGanadores')}
             className='bg-yellow-300 hover:bg-yellow-400 p-2 rounded-md font-semibold'>Ingresar</button>
         </div>
       </article>
